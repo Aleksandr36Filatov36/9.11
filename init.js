299bc6272ef13adb11fef703dcaebf55179270b9
Александр36Филатов36 <678987123@rambler.ru>
2023-01-14 21:10:20 +0300

    firstVersion

---
+++ init.js
@@ -0,0 +1,10 @@
+window.onload = function()
+{
+    const initPerson = personGenerator.getPerson();
+    document.getElementById("surnameOutput").innerText = "Генерация фамилии";
+    document.getElementById("firstNameOutput").innerText = "Генерация имени";
+    document.getElementById("patronymicOutput").innerText = "Генерация фамилии";
+    document.getElementById("genderOutput").innerText = "Генерация пола";
+    document.getElementById("birthYearOutput").innerText = 'Генерация года рождения';
+    document.getElementById("occupationOutput").innerText = 'Генерация професии';
+};
